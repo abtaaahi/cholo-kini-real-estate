4ac08822374b9af1cac15116d4d587aa1ec2e8e6
abtaaahi <abtahimahib@gmail.com>
2024-11-18 22:37:54 +0600

    pictures added

--- src/components/InteriorSolution.js
+++ src/components/InteriorSolution.js
@@ -9,14 +9,22 @@ const imageArrayHouse = [
   'https://res.cloudinary.com/dasqmi9fl/image/upload/v1731263349/IMG-20241110-WA0012_bqmxkq.jpg',
   'https://res.cloudinary.com/dasqmi9fl/image/upload/v1731263350/IMG-20241110-WA0002_txpcth.jpg',
   'https://res.cloudinary.com/dasqmi9fl/image/upload/v1731263348/IMG-20241110-WA0009_ngylfz.jpg',
-  'https://res.cloudinary.com/dasqmi9fl/image/upload/v1731263351/IMG-20241110-WA0005_wh41m1.jpg'
+  'https://res.cloudinary.com/dasqmi9fl/image/upload/v1731263351/IMG-20241110-WA0005_wh41m1.jpg',
+  'https://res.cloudinary.com/dasqmi9fl/image/upload/v1731947405/IMG-20241118-WA0007_zxtlpm.jpg',
+  'https://res.cloudinary.com/dasqmi9fl/image/upload/v1731947720/IMG-20241118-WA0005_vxcxze.jpg',
+  'https://res.cloudinary.com/dasqmi9fl/image/upload/v1731947722/IMG-20241118-WA0004_flgvks.jpg',
+  'https://res.cloudinary.com/dasqmi9fl/image/upload/v1731947728/IMG-20241118-WA0009_cs1id1.jpg'
 ];
 
 const imageArrayOffice = [
   'https://res.cloudinary.com/dasqmi9fl/image/upload/v1731263291/IMG-20241110-WA0016_tf56fh.jpg',
+  'https://res.cloudinary.com/dasqmi9fl/image/upload/v1731263291/IMG-20241110-WA0014_fpyrji.jpg',
+  'https://res.cloudinary.com/dasqmi9fl/image/upload/v1731947403/IMG-20241118-WA0002_fs4rjo.jpg',
   'https://res.cloudinary.com/dasqmi9fl/image/upload/v1731263291/IMG-20241110-WA0013_yv4rfq.jpg',
+  'https://res.cloudinary.com/dasqmi9fl/image/upload/v1731947402/IMG-20241118-WA0008_tcch83.jpg',
   'https://res.cloudinary.com/dasqmi9fl/image/upload/v1731263291/IMG-20241110-WA0015_xs5di0.jpg',
-  'https://res.cloudinary.com/dasqmi9fl/image/upload/v1731263291/IMG-20241110-WA0014_fpyrji.jpg'
+  'https://res.cloudinary.com/dasqmi9fl/image/upload/v1731947560/IMG-20241118-WA0003_jrn06s.jpg',
+  'https://res.cloudinary.com/dasqmi9fl/image/upload/v1731947643/IMG-20241118-WA0006_l0scbt.jpg'
 ];
 
 const InteriorSolution = () => {
